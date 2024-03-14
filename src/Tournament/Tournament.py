a601e122dd6172c409e455dce3912ac51c163f04
Marcus <hoffmannmarcus2@gmail.com>
2024-03-14 18:42:57 +0100

    first draft of Gameplan interface for tournaments

--- src/Tournament/Tournament.py
+++ src/Tournament/Tournament.py
@@ -3,46 +3,56 @@ from Entities.Team import Team
 from Entities.Player import Player
 from Game.Game import *
 from abc import ABC, abstractmethod
-from typing import Protocol
+
+from Game.Game import Game
 
 class Phase_Type(Enum):
     Single_Knockout= "Single_Knockout"
     Double_Knockout= "Double_Knockout"
     Group_Stage= "Group_Stage"
 
-class GamePlan(Protocol):
-    def get_next_game(team: Team) -> Game:
-        pass
-
-    def is_PhaseRound_finished() -> bool:
-        pass
-    
-
-
-@dataclass
-class Phase(ABC):
-    
+class GamePlan(ABC):
     input_player: list[Team]
     output_player: list[Team]
-    type: Phase_Type
-    game_plan: GamePlan
 
     @abstractmethod
-    def create_game_plan(self):
+    def get_games(self, playable:bool) -> list[Game]:
         pass
-    
 
+    @abstractmethod
+    def register_game_result(self, finished_game: Game):
+        pass
+
+    @abstractmethod
+    def determine_output_player(self):
+        pass
+
+    @abstractmethod
+    def get_current_result(self) -> list[Team]:
+        pass
+
+
+class Group(GamePlan):
+    name: str
+    matches_played: dict[Team:set[Team]]
+    standings: list[Team]
+    placement_to_advance: int
+
+class GroupStage(GamePlan):
+    groups: list[Group]
+    placement_to_advance: int
+    
 
 class Tournament():
     registered_teams: list[Team]
     id: int
     name: str
-    phases: list[Phase]  
+    phases: list[GamePlan]  
     # start_time: datetime
     # end_time: datetime
     winner: Team
 
-    def __init__(self,registered_teams,id,name,phases):
+    def __init__(self,registered_teams: list[Team], id:int, name: str, phases: list[GamePlan]):
         self.registered_teams= registered_teams
         self.id= id
         self.name = name

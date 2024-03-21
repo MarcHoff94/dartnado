afb1be87da9cd74b4c02d3792549cad5a5159684
Christoph <cgh.hermann@gmail.com>
2024-03-21 23:23:40 +0100

    started to play around with tournament tree

--- src/test.py
+++ src/test.py
@@ -14,6 +14,14 @@ class Game () :
     player2: int
     winner: None
 
+    def __init__(self,team1,team2) -> None:
+        self.team1= team1
+        self.team2= team2
+    
+    def __str__(self) -> str:
+        return f"Team 1: {self.team1}, Team 2: {self.team2}"
+
+
     def create_game():
         return 
 
@@ -31,27 +39,56 @@ class Tournament_Tree():
         return int(round(math.log2(self.no_players),0))
     
 
-
-
-
-
-
+teamlist= [1,2,3,4]
 
 tournament= Tournament_Tree(8)
 
-instances = []
-for rounds in range(tournament.number_of_rounds):
-    print(rounds)
-    no_players_per_round= 2 ** rounds
-    for x in range(no_players_per_round):
-        instances['Game' + str(x)] = Game()
+instances = {}
+# for rounds in range(tournament.number_of_rounds,1,-1):
+#     print(rounds)
+no_teams_per_round= 8#2 ** rounds
+for x,z in enumerate(range( no_teams_per_round,int(no_teams_per_round/2),-1)): 
+    game_id= 'Game'+ str(x)
+    instances[game_id] = Game(team1=x+1,team2=z)
 
-print(instances.items())
-print(tournament.number_of_rounds)
-
-
-
+for instance in instances:
+    print(instances[instance])
 
 
 
+# import math
+
+# def tournament_round( no_of_teams , matchlist ):
+#     new_matches = []
+#     for team_or_match in matchlist:
+#         if type(team_or_match) == type([]):
+#             new_matches += [ tournament_round( no_of_teams, team_or_match ) ]
+#         else:
+#             new_matches += [ [ team_or_match, no_of_teams + 1 - team_or_match ] ]
+#     return new_matches
+
+# def flatten_list( matches ):
+#     teamlist = []
+#     for team_or_match in matches:
+#         if type(team_or_match) == type([]):
+#             teamlist += flatten_list( team_or_match )
+#         else:
+#             teamlist += [team_or_match]
+#     return teamlist
+
+# def generate_tournament( num ):
+#     num_rounds = math.log( num, 2 )
+#     if num_rounds != math.trunc( num_rounds ):
+#         raise ValueError( "Number of teams must be a power of 2" )
+#     teams = 1
+#     result = [1]
+#     while teams != num:
+#         teams *= 2
+#         result = tournament_round( teams, result )
+#     return result
+#     #return flatten_list( result )
+
+
+# tournament= generate_tournament(32)
+# print(tournament)
 

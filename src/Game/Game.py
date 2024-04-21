0352a19e78576164961cd299088c72a8ff273a31
MarcHoff94 <hoffmannmarcus2@gmail.com>
2024-04-21 22:11:09 +0200

    restructured and changed Game class game no longer registers round, instead now every throw is registered and Game handles the entire logic

--- src/Game/Game.py
+++ src/Game/Game.py
@@ -1,15 +1,13 @@
-from dataclasses import dataclass
 from enum import Enum
 from pydantic import BaseModel, Field
 from abc import ABC, abstractmethod
 from Entities.Team import Team
-
+import random
 
 class GameStatus(str, Enum):
     finished= "finished"
     ongoing= "ongoing"
     interrupted= "interrupted"
-
 class Multiplier(int, Enum):
     SINGLE= 1
     DOUBLE= 2
@@ -19,20 +17,36 @@ class Throw(BaseModel):
     multiplier: Multiplier
     value: int
 
+class Check_Out(str, Enum):
+    STRAIGHT_OUT= "STRAIGHT OUT"
+    DOUBLE_OUT= "DOUBLE OUT"
+    MASTER_OUT= "MASTER OUT"
+class Check_In(str, Enum):
+    STRAIGHT_IN= "STRAIGHT IN"
+    DOUBLE_IN= "DOUBLE IN"
+    TRIPLE_IN= "TRIPLE IN"
+
+
 class GameRound(BaseModel):
     round: list[Throw]
+    checked_in: bool = Field(False)
     team_id: int
     player_id: int
 
-    def register_throw(self, new_throw:Throw) -> int | None:
+    def register_throw(self, new_throw:Throw, check_in_condition: Check_In) -> int | None:
         """Stores parameter new_throw and return number of throws remaining in this round"""
+        if self.checked_in == False:
+            if meets_check_in_condition(check_in_condition, new_throw):
+                self.checked_in = True
+            else:
+                new_throw.value = 0
+                
         self.round.append(new_throw)
         remaining_throws = 3 - len(self.round)
         if remaining_throws > 0:
             return remaining_throws
         else:
             None
-
 
 class Leg(BaseModel):
     points: dict[int,int]  #key = team.id
@@ -67,18 +81,6 @@ class Set(BaseModel):
         return len(self.legs[team_id])
 
 
-class Check_Out(str, Enum):
-    STRAIGHT_OUT= "STRAIGHT OUT"
-    DOUBLE_OUT= "DOUBLE OUT"
-    MASTER_OUT= "MASTER OUT"
-
-
-class Check_In(str, Enum):
-    STRAIGHT_IN= "STRAIGHT IN"
-    DOUBLE_IN= "DOUBLE OUT"
-    TRIPLE_IN= "SINGLE OUT"
-
-
 class Game_Mode(BaseModel):
     sets_to_win: int
     legs_to_win_set: int
@@ -103,7 +105,8 @@ class Game(BaseModel, UserInterface):
     sets: dict[int,list[Set]] #key = team.id
     current_set: Set
     current_leg: Leg
-    starts_leg: int #team.id
+    current_gameround: GameRound
+    started_leg: int #team.id
     winner: Team | None = Field(None)
 
     # start_time: datetime
@@ -111,24 +114,83 @@ class Game(BaseModel, UserInterface):
     # standings: str = None
     class Config:
         arbitrary_types_allowed = True
-    
-    def register_round(self, new_round: GameRound) -> Leg | None:
-        """registers round in game and returns the next leg that is gonna be played. If the game is over, None is returned"""
-        result = self.current_leg.register_round(new_round)
-        if result == 0:
-            self.current_set.register_finished_leg(new_round.team_id, self.current_leg)
+            
+    def meets_check_out_condition(self, throw: Throw) -> bool:
+        match self.game_mode.check_out.value:
+            case 'STRAIGHT OUT':
+                return True
+            case 'DOUBLE OUT':
+                return throw.multiplier.value == 2
+            case 'MASTER OUT':
+                return (throw.multiplier.value == 2) | (throw.multiplier.value == 3)
 
-            if self.current_set.get_number_of_legs_won(new_round.team_id) == self.game_mode.legs_to_win_set:
-                self.sets[new_round.team_id].append(self.current_set)
 
-                if self.get_number_of_sets_won(new_round.team_id) == self.game_mode.sets_to_win:
-                    self.winner = self.teams[new_round.team_id]
-                    return None
-                self.current_set = Set(dict.fromkeys(self.teams.keys()))
+    def meets_check_in_condition(self, throw: Throw) -> bool:
+        match self.game_mode.check_in.value:
+            case 'STRAIGHT IN':
+                self.current_gameround.checked_in = True
+                return True
+            case 'DOUBLE IN':
+                if throw.multiplier.value == 2:
+                    self.current_gameround.checked_in = True
+                    return True
+                else:
+                    return False
+            case 'TRIPLE IN':
+                if throw.multiplier.value == 3:
+                    self.current_gameround.checked_in = True
+                    return True
+                else:
+                    return False
+    def is_game_won(self) -> bool:
+        if self.current_set.get_number_of_legs_won(self.current_gameround.team_id) == self.game_mode.legs_to_win_set:
+            self.sets[self.current_gameround.team_id].append(self.current_set)
 
-            self.current_leg = Leg(points=dict.fromkeys(self.teams.keys()), rounds=dict.fromkeys(self.teams.keys()))
-        return self.current_leg
-    
+            if self.get_number_of_sets_won(self.current_gameround.team_id) == self.game_mode.sets_to_win:
+                self.winner = self.teams[self.current_gameround.team_id]
+                return True
+            self.current_set = Set(dict.fromkeys(self.teams.keys()))
+
+        self.current_leg = Leg(points=dict.fromkeys(self.teams.keys()), rounds=dict.fromkeys(self.teams.keys()))
+        return False
+       
+    def register_throw(self, new_throw: Throw):
+
+        if self.current_gameround.checked_in == False:
+            if self.meets_check_in_condition(new_throw) == False:
+                new_throw.value = 0
+
+        result = self.current_leg.points[self.current_gameround.team_id] - new_throw.value
+        if result < 0:
+            new_throw.value = 0
+            self.current_gameround.round.append(new_throw)
+            while self.current_gameround.round.__len__() < 3:
+                self.current_gameround.round.append(Throw(Multiplier.SINGLE, 0))   
+        elif result == 0 & self.meets_check_out_condition(new_throw):
+            self.current_leg.points[self.current_gameround.team_id] = result
+            self.current_gameround.round.append(new_throw)
+            self.current_leg.register_round(self.current_gameround)
+            self.current_set.register_finished_leg(self.current_gameround.team_id, self.current_leg)
+            return
+        else:
+            self.current_leg.points[self.current_gameround.team_id] = result
+
+
+        if 3 - self.current_gameround.round.__len__() == 0:
+            self.current_leg.register_round(self.current_gameround)
+            #hier weiter machen, das team das nicht gespielt hat is als nächstes dran (auch noch in elif in Zeile 188 einfügen)
+            next_team = 0
+            self.current_gameround = GameRound(round=list(), checked_in=False, team_id=next_team, player_id=0)  
+        
+        
+        
+            
+    def register_gameround(self):
+        pass
+    def start_game(self):
+        self.started_leg = random.choice(list(self.teams.keys()))
+        self.current_gameround = GameRound(round=list(), checked_in=False, team_id=self.started_leg)
+
     def get_number_of_sets_won(self, team_id: int) -> int:
         return len(self.sets[team_id])
     
--- src/test.py
+++ src/test.py
@@ -5,10 +5,10 @@ import time
 import requests
 from Game.Game import *
 from Entities.Player import Player
+#Dummy stuff created
+dartboard_values = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,25,50]
 
-dartboard_values = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,25,50]
-
-gamemode = Game_Mode(sets_to_win=3,legs_to_win_set=3,points_per_leg=501, check_out=Check_Out.DOUBLE_OUT, check_in=Check_In.STRAIGHT_IN)
+gamemode = Game_Mode(sets_to_win=3,legs_to_win_set=3,points_per_leg=501, check_out=Check_Out.DOUBLE_OUT, check_in=Check_In.DOUBLE_IN)
 team_1 = Team(id=1, team_name='team_1', players=[Player(id=1, name='Christoph Hermann', nickname= 'Hermanndez', walk_on_music='https://www.youtube.com/watch?v=xlacPkRfVhg')])
 team_2 = Team(id=2, team_name='team_2', players=[Player(id=2, name='Marcus Hoffmann', nickname='MarcHoff94', walk_on_music='https://www.youtube.com/watch?v=dQw4w9WgXcQ')])
 teams = {team_1.id: team_1, team_2.id: team_2}
@@ -24,10 +24,11 @@ game_1 = Game(
     )
 tournament_id = 1
 
+#Building interactive UI
 put_text('Getting game from tournament server...')
 with put_loading():
     
-    url = f'http://localhost:8000/tournament/{tournament_id}/game/{5}'
+    url = f'http://localhost:8000/tournament/{tournament_id}/game/{game_1.game_id}'
     response = requests.get(url)
     if response.status_code != 200:
         put_text(f'Warning! could not receive game from server. Status code from response: {response.status_code}')
@@ -61,7 +62,7 @@ while game_finished == False:
         ])
         new_throw = Throw(multiplier=throw_data['multiplier'], value=throw_data['points'])
         
-        if last_round_played.register_throw(new_throw) == None:
+        if last_round_played.register_throw(new_throw, game_1.game_mode.check_in) == None:
             round_finished = True
             nth_throw = 0
 
